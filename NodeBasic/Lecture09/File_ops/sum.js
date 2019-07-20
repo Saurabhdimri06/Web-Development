4abce7d4645d94dbe0a48fe32e4aaf7fec69d348
Saurabh Dimri <dimrisaurabh06@gmail.com>
2019-07-21 00:06:16 +0530

    Let's work on some array functions

---
+++ NodeBasic/Lecture09/File_ops/sum.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) 
+        return console.log(err)
+    
+    const result = data.split('\n')
+        .filter(el => !isNaN(el))
+        .map(el => +el)
+        .reduce((acc, val) => acc + val, 0)
+
+    fs.writeFile('./output.txt', result, (err) => {
+        if (err)
+            return console.log(err)
+
+        console.log("Done")
+    })
+})
+
+app.listen(3000);
+
